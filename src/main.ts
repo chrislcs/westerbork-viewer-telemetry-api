4136df5d6dd7adca1898af459a609125eef0be71
Chris Lucas <chris.lucas@geodan.nl>
2022-05-01 18:27:31 +0200

    add api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
